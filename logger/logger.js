b15bfca27b9024a762361dfa290f870fdefa5e5b
Vitaliy <prokinvs@gmail.com>
2021-05-18 00:14:28 +0300

    asfd
    zvcx

--- logger/logger.js
+++ logger/logger.js
@@ -3,20 +3,38 @@ const log4js = require(`log4js`);
 
 log4js.configure({
     appenders: {
-        voip: {
+        access: {
             type: `file`,
-            filename: `logs/debug.log`,
+            filename: `logs/access.log`,
+            maxLogSize: 10485760,
+            backups: 3,
+            compress: true
+        },
+        error: {
+            type: `file`,
+            filename: `logs/error.log`,
             maxLogSize: 10485760,
             backups: 3,
             compress: true
         }
     },
     categories: {
-        default: {
-            appenders: [`voip`],
-            level: `debug`
+        default: { appenders: ['access'], level: 'info' },
+        access: {
+            appenders: [`access`],
+            level: `info`
+        },
+        error: {
+            appenders: [`error`],
+            level: `error`
         }
     }
 });
-const logger = log4js.getLogger(`voip`);
-module.exports = logger;
+
+const access = log4js.getLogger('access');
+const error = log4js.getLogger('error');
+
+module.exports = {
+    access,
+    error
+};
