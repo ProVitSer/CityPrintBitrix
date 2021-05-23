4390d4c12e2e2245214c4bf62699dd2361f61a95
Vitaliy <prokinvs@gmail.com>
2021-05-23 20:41:02 +0300

    Добавлены методы работы с задачами

--- src/bitrix.js
+++ src/bitrix.js
@@ -24,12 +24,12 @@ class Bitrix extends Axios {
         };
 
         try {
-            const result = await this.post(`${this.domain}/${this.hash}/telephony.externalcall.register.json`, json)
+            const result = await this.post(`${this.domain}/${this.hash}/telephony.externalcall.register.json`, json);
             return result.data.result;
         } catch (e) {
             return e;
         }
-    };
+    }
 
 
     async externalCallFinish(...params) {
@@ -44,12 +44,12 @@ class Bitrix extends Axios {
 
 
         try {
-            const result = await this.post(`${this.domain}/${this.hash}/telephony.externalcall.finish`, json)
+            const result = await this.post(`${this.domain}/${this.hash}/telephony.externalcall.finish`, json);
             return result.data.result;
         } catch (e) {
             return e;
         }
-    };
+    }
 
     async createTask(...params) {
         const daedline = moment(new Date).add(bitrixConfig.bitrix.daedlineMin, 'minutes').format('YYYY-MM-DD H:mm:ss');
@@ -60,53 +60,109 @@ class Bitrix extends Axios {
                 "CREATED_BY": bitrixConfig.bitrix.userTaskId,
                 "DESCRIPTION": `Пропущенный вызов от абонента ${params[1]}`,
                 "PRIORITY": "2",
+                "GROUP_ID": bitrixConfig.bitrix.taskGroup,
                 "DEADLINE": daedline
             }
         }
 
         try {
-            const result = await this.post(`${this.domain}/${this.hash}/tasks.task.add`, json)
+            const result = await this.post(`${this.domain}/${this.hash}/tasks.task.add`, json);
             return result.data.result;
         } catch (e) {
             return e;
         }
-    };
+    }
 
-    async taskStatus(...params) {
+    async userGet() {
+        try {
+            const result = await this.get(`${this.domain}/${this.hash}/user.get`);
+            return result.data.result;
+        } catch (e) {
+            return e;
+        }
+    }
+
+    async checkTaskStatus(id) {
         const json = {
-            "taskId": params[0]
+            "taskId": id
         }
 
         try {
-            const result = await this.post(`${this.domain}/${this.hash}/tasks.task.get`, json)
-            if (result.task.status == '2') {
-                logger.access.info(`Задача просрочена ${params[0]}`);
-                this.updateTaskResponsibleId(params[0]);
+
+            const resultGet = await this.post(`${this.domain}/${this.hash}/tasks.task.get`, json);
+            if (resultGet.data.result.task.status == '2') {
+                logger.access.info(`Задача просрочена ${id}`);
+                this.addAuditorsToTask(id);
             }
             return;
 
         } catch (e) {
             return e;
         }
-    };
+    }
 
-    async updateTaskResponsibleId(...params) {
+    async getTaskStatus(id) {
         const json = {
-            "taskId": params[0],
+            "taskId": id
+        }
+
+        try {
+            const resultGetTask = await this.post(`${this.domain}/${this.hash}/tasks.task.get`, json);
+            if (resultGetTask.status == 400) {
+                return false;
+            }
+            return resultGetTask.data.result.task
+        } catch (e) {
+            return e;
+        }
+    }
+
+    async addAuditorsToTask(id) {
+        const json = {
+            "taskId": id,
             "fields": {
-                "auditors": [bitrixConfig.bitrix.adminId]
+                "AUDITORS": [bitrixConfig.bitrix.adminId]
+            }
+        }
+        try {
+            const result = await this.post(`${this.domain}/${this.hash}/tasks.task.update`, json);
+            logger.access.info(`Добавление наблюдателя по задаче ${util.inspect(result)}`);
+        } catch (e) {
+            logger.error.error(e);
+        }
+    }
+
+
+    async closeTask(id) {
+        const json = {
+            "taskId": id,
+            "fields": {
+                "STATUS": bitrixConfig.bitrix.taskIdClose
+            }
+        }
+        try {
+            const result = await this.post(`${this.domain}/${this.hash}/tasks.task.update`, json);
+            logger.access.info(`Результат завершение задачи ${util.inspect(result)}`);
+        } catch (e) {
+            logger.error.error(e);
+        }
+    }
+
+    async updateResponsibleIdTask(taskId, bitrixId) {
+        const json = {
+            "taskId": taskId,
+            "fields": {
+                "RESPONSIBLE_ID": bitrixId
             }
         }
 
         try {
-            const result = await this.post(`${this.domain}/${this.hash}/tasks.task.update`, json)
-            logger.access.info(`Добавление наблюдателя по задаче ${util.inspect(result)}`);
-
+            const result = await this.post(`${this.domain}/${this.hash}/tasks.task.update`, json);
+            logger.access.info(`Обновление отвесттвенного по задаче ${util.inspect(result)}`);
         } catch (e) {
             logger.error.error(e);
         }
-    };
-
+    }
 
 
 };
